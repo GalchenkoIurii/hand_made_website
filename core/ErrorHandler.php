1118eba690d6a4a19068854d6677beee6d76dde3
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2020-09-14 10:29:25 +0300

    ErrorHandler, added errors pages

--- core/ErrorHandler.php
+++ core/ErrorHandler.php
@@ -23,7 +23,9 @@ class ErrorHandler
 
     public function exceptionHandler($e)
     {
-
+        $this->logErrors($e->getMessage(), $e->getFile(), $e->getLine());
+        $this->displayError('Exception', $e->getMessage(), $e->getFile(),
+            $e->getLine(), $e->getCode());
     }
 
     protected function logErrors($message = '', $file = '', $line = '')
@@ -36,5 +38,15 @@ class ErrorHandler
     protected function displayError($errno, $errstr, $errfile, $errline, $response = 404)
     {
         http_response_code($response);
+        if ($response == 404 && !DEBUG) {
+            require_once WWW . '/errors/404.php';
+            die;
+        }
+        if (DEBUG) {
+            require_once WWW . '/errors/dev.php';
+        } else {
+            require_once WWW . '/errors/prod.php';
+        }
+        die;
     }
 }
