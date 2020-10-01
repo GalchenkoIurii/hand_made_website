980412199df002765f48f856d931266a64cacbc8
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2020-10-01 16:12:13 +0300

    loading form data via ajax and front-end form validation

--- public/js/formValidation.js
+++ public/js/formValidation.js
@@ -1,11 +1,11 @@
-(function() {
+(function () {
     'use strict';
-    window.addEventListener('load', function() {
+    window.addEventListener('load', function () {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = document.getElementsByClassName('needs-validation');
         // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener('submit', function(event) {
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
                 if (form.checkValidity() === false) {
                     event.preventDefault();
                     event.stopPropagation();
@@ -14,17 +14,24 @@
                         url: 'index.php',
                         type: 'POST',
                         data: $('#form').serialize(),
-                        beforeSend: function() {
+                        beforeSend: function () {
                             $('.loader').fadeIn();
                         },
-                        success: function(response) {
-                            $('.loader').fadeOut('slow', function() {
-                                console.log(response);
+                        success: function (response) {
+                            $('.loader').fadeOut('slow', function () {
                                 let result = JSON.parse(response);
-                                console.log(result);
+                                if (result.answer == 'ok') {
+                                    $('#form').removeClass('was-validated').trigger('reset');
+                                    $('#captcha').text(result.captcha);
+                                    $('#answer').html(`<div class="alert alert-success" role="alert">
+                                        Ваше сообщение отправлено. Я с Вами свяжусь в ближайшее время.</div>`);
+                                } else {
+                                    $('#answer').html(`<div class="alert alert-danger" role="alert">
+                                        ${result.errors}</div>`);
+                                }
                             });
                         },
-                        error: function() {
+                        error: function () {
                             alert('Error');
                         }
                     });
